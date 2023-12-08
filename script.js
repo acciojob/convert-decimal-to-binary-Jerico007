904b7db37bd1e61ae9b4a0ac8936fbba8b2f0ef2
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-12-08 12:02:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function decimalToBinary(num) {
   // your code here
+	if(num === 1)
+	{
+		return 1;
+	}
+	if(num === 2)
+	{
+		return 10;
+	}
+	while (num > 1) {
+		
+	}
 }
-const num = parseInt(prompt("Enter a number."));
+// const num = parseInt(prompt("Enter a number."));
 alert(decimalToBinary(num));
